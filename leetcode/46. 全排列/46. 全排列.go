54df780e0e452508f866c307abb545e8fff43df8
sky1309 <549471729@qq.com>
2024-05-22 13:39:38 +0800

    add: 46. 全排列

---
+++ leetcode/46. 全排列/46. 全排列.go
@@ -0,0 +1,67 @@
+package main
+
+/*
+46. 全排列
+中等
+给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+示例 2：
+
+输入：nums = [0,1]
+输出：[[0,1],[1,0]]
+示例 3：
+
+输入：nums = [1]
+输出：[[1]]
+
+
+提示：
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+nums 中的所有整数 互不相同
+*/
+
+func permute(nums []int) [][]int {
+	n := len(nums)
+	var ans [][]int
+
+	var path []int
+	selectedMap := make(map[int]bool)
+
+	var dfs func(i int)
+	dfs = func(i int) {
+		// 找到答案了
+		if i == n {
+			row := make([]int, len(path))
+			copy(row, path)
+			ans = append(ans, row)
+			return
+		}
+
+		// 枚举选哪个
+		for _, x := range nums {
+			// 假如之前已经选过了，跳过
+			if _, ok := selectedMap[x]; ok {
+				continue
+			}
+
+			path = append(path, x)
+			selectedMap[x] = true
+			dfs(i + 1)
+
+			// 回溯，清理现场
+			path = path[:len(path)-1]
+			delete(selectedMap, x)
+		}
+	}
+
+	dfs(0)
+	return ans
+}
