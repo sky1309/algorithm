232a85d4365e252a7edef9d36cf8a094ed385fe1
sky1309 <549471729@qq.com>
2024-05-27 09:56:29 +0800

    add: 560. 和为 K 的子数组

---
+++ leetcode/560. 和为 K 的子数组/560. 和为 K 的子数组.go
@@ -0,0 +1,44 @@
+package main
+
+/**
+560. 和为 K 的子数组
+给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+
+子数组是数组中元素的连续非空序列。
+
+
+
+示例 1：
+
+输入：nums = [1,1,1], k = 2
+输出：2
+示例 2：
+
+输入：nums = [1,2,3], k = 3
+输出：2
+
+
+提示：
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+*/
+
+func subarraySum(nums []int, k int) int {
+	// []int{1, 2, 3} 3
+
+	ans := 0
+	hash := make(map[int]int)
+	hash[0] = 1
+	pre := 0
+	for _, x := range nums {
+		pre += x
+		if cnt, ok := hash[pre-k]; ok {
+			ans += cnt
+		}
+		hash[pre]++
+	}
+
+	return ans
+}
